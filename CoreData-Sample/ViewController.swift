3ce9f601b7028a4ddb50cacdf86fb131aa39a4f6
Javid Museyibli <javid.museyibli@pashabank.az>
2020-04-19 14:24:41 +0400

    Initial Commit

---
+++ CoreData-Sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreData-Sample
+//
+//  Created by Javid Museyibli on 4/19/20.
+//  Copyright © 2020 Javid Museyibli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
